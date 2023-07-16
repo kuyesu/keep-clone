b7686bd02d412069b944398f8b4fe81d1ebd625e
kuyesu <kuyesu22@gmail.com>
2023-07-16 19:37:10 +0300

    nav and changes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,9 +4,66 @@ import Home from "../Pages/Home";
 import Note from "../Pages/note"
 export default function Index() {
   return (
-      <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/note/:id' element={<Note/>} />
-    </Routes>
-  )
+    <div
+      style={{
+        display: "grid",
+        gridTemplateRows: "10vh 80vh 10vh",
+      }}
+    >
+      {/* NavBar 1 */}
+      <div
+        style={{
+          border: "solid 1px black",
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/note/:id" element={<Note />} />
+        </Routes>
+      </div>
+      {/* Main section   */}
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "30vw 70vw",
+          border: "solid 1px black",
+        }}
+      >
+          {/* sidebar */}
+        <div
+          style={{
+            border: "solid 1px black",
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/note/:id" element={<Note />} />
+          </Routes>
+        </div>
+        {/* content */}
+        <div
+          style={{
+            border: "solid 1px black",
+          }}
+        >
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/note/:id" element={<Note />} />
+          </Routes>
+        </div>
+      </div>
+      {/* footer */}
+      <div
+        style={{
+          border: "solid 1px black",
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/note/:id" element={<Note />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
